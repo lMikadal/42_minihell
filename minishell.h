a3596fdd2da3b47a1df9ebb045b50165b2b78615
Panupong Mikada <pmikada@z3t1c2.42bangkok.com>
2022-12-19 15:41:07 +0700

    add include

--- minishell.h
+++ minishell.h
@@ -6,12 +6,24 @@
 /*   By: pmikada <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 13:21:53 by pmikada           #+#    #+#             */
-/*   Updated: 2022/12/19 13:28:01 by pmikada          ###   ########.fr       */
+/*   Updated: 2022/12/19 15:40:23 by pmikada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <dirent.h>
+# include <string.h>
+# include <sys/ioctl.h>
+# include <term.h>
 
 #endif
